75262841d1d39a1d7f984c5ea64efada4b11a542
Liora Milbaum <lmilbaum@redhat.com>
2025-09-16 19:31:54 +0300

    refactored

--- main.py
+++ main.py
@@ -1,57 +1,47 @@
-#!/usr/bin/env python3
-
-import confluent_kafka
+import sys
+import six
 import dotenv
 import os
 
+if sys.version_info >= (3, 12, 0):
+    sys.modules["kafka.vendor.six.moves"] = six.moves
+from kafka import KafkaConsumer
 
-def process_message(msg):
-    data = msg.value().decode("utf-8")
-    print("Processing message:", data)
+# These would come from env variables and/or secrets.
+kafka_hosts = [
+    "b-3.itsandbox.642qp2.c5.kafka.us-east-1.amazonaws.com:9096",
+    "b-1.itsandbox.642qp2.c5.kafka.us-east-1.amazonaws.com:9096",
+    "b-2.itsandbox.642qp2.c5.kafka.us-east-1.amazonaws.com:9096",
+]
+topic = ""
+user_name = ""
+password = ""
 
 
 def main():
+    print("Connecting to Kafka...")
+
     dotenv.load_dotenv()
 
-    conf = {
-        "bootstrap.servers": os.getenv("BOOTSTRAP_SERVERS"),
-        "group.id": os.getenv("GROUP_ID"),
-        "auto.offset.reset": os.getenv("AUTO_OFFSET_RESET"),
-        "security.protocol": os.getenv("SECURITY_PROTOCOL"),
-        "sasl.mechanisms": os.getenv("SASL_MECHANISMS"),
-        "sasl.username": os.getenv("SASL_USERNAME"),
-        "sasl.password": os.getenv("SASL_PASSWORD"),
-    }
+    # generating the Kafka Consumer
+    my_consumer = KafkaConsumer(
+        os.getenv("KAFKA_TOPIC"),
+        bootstrap_servers=os.getenv("BOOTSTRAP_SERVERS"),
+        auto_offset_reset="earliest",
+        security_protocol="SASL_SSL",
+        sasl_mechanism="SCRAM-SHA-512",
+        sasl_plain_username=os.getenv("SASL_USERNAME"),
+        sasl_plain_password=os.getenv("SASL_PASSWORD"),
+        enable_auto_commit=True,
+        api_version=(3, 7, 0),
+    )
 
-    consumer = confluent_kafka.Consumer(conf)
+    print("Connected!")
 
-    topic = os.getenv("KAFKA_TOPIC")
+    for message in my_consumer:
+        print(str(message.value, encoding="utf-8"))
 
-    try:
-        consumer.subscribe([topic])
-    except Exception as e:
-        print(e)
-
-    print(f"Subscribed to topic: {topic}")
-    print("Waiting for messages... Press Ctrl+C to exit.")
-
-    try:
-        while True:
-            msg = consumer.poll(1.0)
-            if msg is None:
-                continue
-            if msg.error():
-                if msg.error().code() == confluent_kafka.KafkaError._PARTITION_EOF:
-                    continue
-                else:
-                    raise confluent_kafka.KafkaException(msg.error())
-
-            process_message(msg)
-
-    except KeyboardInterrupt:
-        print("\nExiting consumer...")
-    finally:
-        consumer.close()
+    my_consumer.close()
 
 
 if __name__ == "__main__":
